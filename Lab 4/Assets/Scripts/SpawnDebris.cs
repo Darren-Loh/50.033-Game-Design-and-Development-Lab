f7023df89ceb65f56d91003d128f09a9b1100c75
Darren Loh <vanhfdarren@gmail.com>
2022-06-07 18:46:29 +0800

    Lab 3 completed

---
+++ Lab 4/Assets/Scripts/SpawnDebris.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDebris : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        	for (int x =  0; x<5; x++){
+			Instantiate(prefab, transform.position, Quaternion.identity);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
